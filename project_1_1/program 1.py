e939c265ab2082f0812d2ab7c96b6a334ef995a4
Korolev Sergey <KorolievSergo@yandex.ru>
2023-01-26 12:42:16 +0300

    Очистили файл

--- project_1_1/program 1.py
+++ project_1_1/program 1.py
@@ -1,14 +1,4 @@
-# for i in range(6):
-#     print("AAA")
-# for j in range(5):
-#     print("BBBB")
-# print("E")
-# for k in range(8):
-#     print("TTTTT")
-# print("G")
-
 # [print(((i * j) + '\n') * k, end='') for i, j, k in zip('ABETG', [3, 4, 1, 5, 1], [6, 5, 1, 9, 1])]
-
 
 # high = int(input())
 # [print('*' * 19) for _ in range(high)]
