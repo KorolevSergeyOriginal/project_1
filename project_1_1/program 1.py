7f869f4a7c98b2b6a912da376f024a13d51f50c4
Korolev Sergey <KorolievSergo@yandex.ru>
2023-01-26 12:34:08 +0300

    Первый коммит

---
+++ project_1_1/program 1.py
@@ -0,0 +1,19 @@
+# for i in range(6):
+#     print("AAA")
+# for j in range(5):
+#     print("BBBB")
+# print("E")
+# for k in range(8):
+#     print("TTTTT")
+# print("G")
+
+# [print(((i * j) + '\n') * k, end='') for i, j, k in zip('ABETG', [3, 4, 1, 5, 1], [6, 5, 1, 9, 1])]
+
+
+# high = int(input())
+# [print('*' * 19) for _ in range(high)]
+
+# m, p, n = [int(input()) for i in range(3)]
+# for i in range(n):
+#     print(i+1, m)
+#     m = float(m + m * p / 100)
